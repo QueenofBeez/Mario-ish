affff45b087296aeec98e477f1a3c5da78a47a76
QueenofBeez <elestoob@gmail.com>
2021-09-13 16:26:53 +0200

    End day 13/09

---
+++ Assets/#Project/Scripts/Elevator.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.transform.parent = transform;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.transform.parent = null;
+        }
+    }
+
+}
